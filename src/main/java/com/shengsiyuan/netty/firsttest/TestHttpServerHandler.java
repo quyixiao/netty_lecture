a8e198d9d4e5146c00cbd773a57c874a1d38be51
quyixiao <2621048238@qq.com>
2020-03-03 16:51:52 +0800

    提交

--- src/main/java/com/shengsiyuan/netty/firsttest/TestHttpServerHandler.java
+++ src/main/java/com/shengsiyuan/netty/firsttest/TestHttpServerHandler.java
@@ -7,16 +7,27 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
+import java.net.URI;
+
 public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
         if (msg instanceof HttpRequest) {
+            HttpRequest httpRequest = (HttpRequest) msg;
+
+            URI uri = new URI(httpRequest.uri());
+            if ("/favicon.ico".equals(uri.getPath())) {
+                return;
+            }
+            System.out.println("请求方法名：" + httpRequest.method().name());
+
             ByteBuf content = Unpooled.copiedBuffer("hello world", CharsetUtil.UTF_8);
             FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
                     HttpResponseStatus.OK, content);
             response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
             response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+            System.out.println("TestHttpServerHandler 执行结束 .... ");
             ctx.writeAndFlush(response);
         }
     }
