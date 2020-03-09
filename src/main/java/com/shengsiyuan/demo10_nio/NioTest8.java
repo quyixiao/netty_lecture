97b543598cacd12edfe0eed220b8321b670a8489
quyixiao <2621048238@qq.com>
2020-03-09 15:42:15 +0800

    提交修改

--- src/main/java/com/shengsiyuan/demo10_nio/NioTest8.java
+++ src/main/java/com/shengsiyuan/demo10_nio/NioTest8.java
@@ -9,30 +9,22 @@ import java.nio.channels.FileChannel;
 public class NioTest8 {
 
     public static void main(String[] args) throws Exception {
-        FileInputStream inputStream = new FileInputStream("input2.txt");
-        FileOutputStream outputStream = new FileOutputStream("output2.txt");
+        FileInputStream inputStream = new FileInputStream("/Users/quyixiao/project/netty_lecture/src/input2.txt");
+        FileOutputStream outputStream = new FileOutputStream("/Users/quyixiao/project/netty_lecture/src/output2.txt");
 
         FileChannel inputChannel = inputStream.getChannel();
         FileChannel outputChannel = outputStream.getChannel();
-
         ByteBuffer buffer = ByteBuffer.allocateDirect(512);
-
-        while(true) {
+        while (true) {
             buffer.clear();
-
             int read = inputChannel.read(buffer);
-
             System.out.println("read: " + read);
-
-            if(-1 == read) {
+            if (-1 == read) {
                 break;
             }
-
             buffer.flip();
-
             outputChannel.write(buffer);
         }
-
         inputChannel.close();
         outputChannel.close();
     }
