90c045d3684d0f05b127a8c8f4225c42dd155987
quyixiao <2621048238@qq.com>
2020-03-10 08:54:24 +0800

    rjuq whnt

--- src/main/java/com/shengsiyuan/demo10_nio/NioTest13.java
+++ src/main/java/com/shengsiyuan/demo10_nio/NioTest13.java
@@ -11,9 +11,10 @@ import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CharsetEncoder;
 
 public class NioTest13 {
-
+    // 将一个字符串，在磁盘上，目标，磁盘存储到我们，字符总是字节一种编码方式，一个字节是一个 byte
+    //
     public static void main(String[] args) {
-        try{
+        try {
             String inputFile = "/Users/quyixiao/project/netty_lecture/src/NioTest13_In.txt";
             String outputFile = "/Users/quyixiao/project/netty_lecture/src/NioTest13_Out.txt";
 
@@ -28,30 +29,29 @@ public class NioTest13 {
             MappedByteBuffer inputData = inputFileChannel.map(FileChannel.MapMode.READ_ONLY, 0, inputLength);
 
             System.out.println("=============");
-
+            // 系统中所有的可用的字符集合
             Charset.availableCharsets().forEach((k, v) -> {
                 System.out.println(k + ", " + v);
             });
-
             System.out.println("=============");
-
+            // 为什么使用 iso-8859-1 就会出现这个问题呢？
             Charset charset = Charset.forName("iso-8859-1");
             CharsetDecoder decoder = charset.newDecoder();
             CharsetEncoder encoder = charset.newEncoder();
 
             CharBuffer charBuffer = decoder.decode(inputData);
 
-        System.out.println(charBuffer.get(12));
+            System.out.println(charBuffer.get(1));
 
             ByteBuffer outputData = encoder.encode(charBuffer);
 
-       //ByteBuffer outputData = Charset.forName("utf-8").encode(charBuffer);
+            //   ByteBuffer outputData = Charset.forName("utf-8").encode(charBuffer);
 
             outputFileChannel.write(outputData);
 
             inputRandomAccessFile.close();
             outputRandomAccessFile.close();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
